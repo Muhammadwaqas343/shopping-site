cf2ea3cb99fad651a573d3735bc20292551f8add
Muhammadwaqas343 <afridwaqas25@gmail.com>
2024-12-03 20:45:49 +0500

    initial scaffolding

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+const Header = () => {
+
+    return (
+        <div>
+            <div>
+                <div>
+                    <h4>Home</h4>
+                    <h4>Men's</h4>
+                    <h4>Women's</h4>
+                    <h4>Kid's</h4>
+                    <h4>Pages</h4>
+                    <h4>Features</h4>
+                    <h4>Explore</h4>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Header;
