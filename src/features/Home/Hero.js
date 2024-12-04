3063ed81ad0676675630f188e9040bbe15e1cc45
Muhammadwaqas343 <afridwaqas25@gmail.com>
2024-12-05 00:49:30 +0500

    full hero

--- src/features/Home/Hero.js
+++ src/features/Home/Hero.js
@@ -1,21 +1,27 @@
 import React from "react";
 import "./Hero.css"
-const Hero =() =>{
+import liftbanner from "../../assets/left-banner-image.jpg"
+import rightba from "../../assets/baner-right-image-01.jpg"
+import rightban from "../../assets/baner-right-image-02.jpg"
+import rightbann from "../../assets/baner-right-image-03.jpg"
+import rightbanner from "../../assets/baner-right-image-04.jpg"
+
+const Hero = () => {
 
     return(
         <div>
-            <div className="main-container">
-                <div className="left">
-                    <p>This is the left Div</p>
-                </div>
-                <div className="right">
-                    <div className="right item">1</div>
-                    <div className="right item">2</div>
-                    <div className="right item">3</div>
-                    <div className="right item">4</div>
-                </div>
-            </div>
+    <div className="main-container">
+        <div className="left">
+            <img src={liftbanner} />
         </div>
+        <div className="right">
+            <img src={rightba} />
+            <img src={rightban} />
+            <img src={rightbann} />
+            <img src={rightbanner} />
+        </div>
+    </div>  
+        </div >
     )
 }
 export default Hero;
